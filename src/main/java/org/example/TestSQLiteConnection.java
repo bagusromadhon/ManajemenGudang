5f1b83f7e09a64b8698a18769939d4e5707997ac
@bagusromadhon007 <bagusromadhon007@gmail.com>
2024-12-23 18:24:19 +0700

    [Update] test code for connection

--- src/main/java/org/example/TestSQLiteConnection.java
+++ src/main/java/org/example/TestSQLiteConnection.java
@@ -1,4 +1,18 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class TestSQLiteConnection {
+    public static void main(String[] args) {
+        String url = "jdbc:sqlite:test.db"; // Nama file database
+
+        try (Connection conn = DriverManager.getConnection(url)) {
+            if (conn != null) {
+                System.out.println("Koneksi SQLite berhasil!");
+            }
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
 }
